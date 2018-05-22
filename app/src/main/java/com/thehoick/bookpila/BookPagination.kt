ba9a353959a590e6f71d4bfba1045a7edbe68e53
Adam Sommer <adam@thehoick.com>
2018-05-22 16:32:57 -0400

    Have ServerBooks paging working as expected.

--- app/src/main/java/com/thehoick/bookpila/BookPagination.kt
+++ app/src/main/java/com/thehoick/bookpila/BookPagination.kt
@@ -3,11 +3,13 @@ package com.thehoick.bookpila
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.android.extension.responseJson
+import org.json.JSONArray
 
-class BookPagination(var layoutManager: LinearLayoutManager, var totalBooks: Int): RecyclerView.OnScrollListener() {
+class BookPagination(var layoutManager: LinearLayoutManager, var totalBooks: Int, var nextUrl: String, var adapter: BooksAdapter): RecyclerView.OnScrollListener() {
     val TAG = BookPagination::class.java.simpleName
-    var isLoading: Boolean = true
-    var isLastPage: Boolean = false
+    var isLoading: Boolean = false
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
@@ -16,13 +18,10 @@ class BookPagination(var layoutManager: LinearLayoutManager, var totalBooks: Int
         val totalItemCount = layoutManager.getItemCount()
         val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
-        Log.d(TAG, "visibleItemCount: $visibleItemCount, totalItemCount: $totalItemCount, firstVisibleItemPosition: $firstVisibleItemPosition")
-        Log.d(TAG, "visibleItemCount + firstVisibleItemPosition: ${visibleItemCount + firstVisibleItemPosition}")
-        Log.d(TAG, "totalItemCount: $totalItemCount, totalBooks: $totalBooks")
-        if (!isLoading && !isLastPage) {
-            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount
-                    && firstVisibleItemPosition >= 0
-                    && totalItemCount >= totalBooks) {
+        if (!isLoading && nextUrl != "null") {
+
+            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount &&
+                totalItemCount >= totalBooks && firstVisibleItemPosition >= totalBooks - 2) {
                 loadMoreBooks(recyclerView)
             }
         }
@@ -34,8 +33,29 @@ class BookPagination(var layoutManager: LinearLayoutManager, var totalBooks: Int
     }
 
     private fun loadMoreBooks(recyclerView: RecyclerView) {
-        Log.d(TAG, "Loading more books...")
-        isLoading = false
-        recyclerView.adapter.notifyDataSetChanged()
+        isLoading = true
+
+        if (nextUrl != "null") {
+            // Get more books.
+            Fuel.get(nextUrl).responseJson { request, response, result ->
+                val newBooks = result.get().obj().get("results") as JSONArray
+                val newBookList = JSONArray()
+
+                nextUrl = result.get().obj().get("next").toString()
+
+                Log.d(TAG, "oldBookList: ${recyclerView.adapter }")
+                for (i in 0..(adapter.bookList!!.length() - 1)) {
+                    newBookList.put(adapter.bookList!![i])
+                }
+                for (i in 0..(newBooks.length() - 1)) {
+                    newBookList.put(newBooks[i])
+                }
+
+                adapter.bookList = newBookList
+                Log.d(TAG, "booksAdapter?.bookList.length(): ${adapter.bookList?.length()}")
+                adapter.notifyDataSetChanged()
+                isLoading = false
+            }
+        }
     }
 }
--- app/src/main/java/com/thehoick/bookpila/ServerBooksFragment.kt
+++ app/src/main/java/com/thehoick/bookpila/ServerBooksFragment.kt
@@ -49,7 +49,6 @@ class ServerBooksFragment: Fragment() {
         val prefs = activity.getSharedPreferences(activity.packageName + "_preferences", 0)
         val url = prefs.getString("url", "")
         val token = prefs.getString("token", "")
-        Log.d(TAG, "getBooks token: $token url: $url")
 
         FuelManager.instance.baseHeaders = mapOf("Authorization" to "Token " + token)
 
@@ -65,63 +64,20 @@ class ServerBooksFragment: Fragment() {
                     message.visibility = INVISIBLE
                     books = result.get().obj().get("results") as JSONArray
                     val booksList = view.findViewById<RecyclerView>(R.id.booksList)
-//                    booksList.layoutManager = LinearLayoutManager(context)
                     val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
                     booksList.layoutManager = linearLayoutManager
                     booksAdapter = BooksAdapter(books)
 
-                    booksList.addOnScrollListener(BookPagination(linearLayoutManager, books.length()))
+                    val nextUrl = result.get().obj().get("next").toString()
 
-//                    val layoutManager = booksList.layoutManager
-//                    var pastVisiblesItems: Int
-//                    var visibleItemCount: Int
-//                    var totalItemCount: Int
-//                    var loading = true
-//
-//                    booksList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//                        override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
-//                            // Check for scroll down.
-//                            if (dy > 0) {
-//                                Log.d(TAG, "dx: $dx, dy: $dy")
-//                                visibleItemCount = recyclerView!!.layoutManager.getChildCount()
-//                                totalItemCount = recyclerView.layoutManager.getItemCount()
-//
-//                                val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager
-//                                val pos = linearLayoutManager.findLastVisibleItemPosition()
-//
-//                                if (pos == totalItemCount - 1) {
-//                                    val nextUrl = result.get().obj().get("next").toString()
-//                                    Log.d(TAG, "At the bottom... nextUrl: $nextUrl")
-//
-//                                    if (nextUrl != "null" && loading) {
-//                                        // Get more books.
-//                                        Fuel.get(nextUrl).responseJson { request, response, result ->
-//                                            val newBooks = result.get().obj().get("results") as JSONArray
-//                                            Log.d(TAG, "newBooks.length(): ${newBooks.length()}")
-//                                            val newBookList = JSONArray()
-//
-//                                            for (i in 0..(booksAdapter!!.bookList!!.length() - 1)) {
-//                                                newBookList.put(booksAdapter!!.bookList!![i])
-//                                            }
-//                                            for (i in 0..(newBooks.length() - 1)) {
-////                                                Log.d(TAG, "newBooks[i]: ${newBooks[i]}")
-//                                                newBookList.put(newBooks[i])
-//                                            }
-//
-////                                            val oldBooks = booksAdapter?.bookList as JSONArray
-//                                            booksAdapter?.bookList = newBookList
-//                                            Log.d(TAG, "booksAdapter?.bookList.length(): ${booksAdapter?.bookList?.length()}")
-//                                            loading = false
-//                                            totalItemCount = totalItemCount + newBooks.length()
-//                                            booksAdapter?.notifyDataSetChanged()
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    })
-//
-//                    booksList.setLayoutManager(LinearLayoutManager(context))
+                    booksList.addOnScrollListener(
+                            BookPagination(
+                                linearLayoutManager,
+                                books.length(),
+                                nextUrl,
+                                booksAdapter!!
+                        )
+                    )
                     booksList.adapter = booksAdapter
                 }
             }
